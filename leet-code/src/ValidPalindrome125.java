65fb48ee3bf42ad93ceab140f7f04b76cf32512a
huangpeng <huangpeng@shalldeal.com>
2019-11-27 15:13:34 +0800

    125. 验证回文串 第二次提交 过滤字符串 + 双指针

--- leet-code/src/ValidPalindrome125.java
+++ leet-code/src/ValidPalindrome125.java
@@ -1,31 +1,40 @@
 public class ValidPalindrome125 {
 
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()){
+        if(s.isEmpty() || s.length() < 2){
+            return true;
+        }
+
+        char[] chars = new char[s.length()];
+        int tail = 0;
+        for (char c : s.toCharArray()){
+            if(!Character.isLetterOrDigit(c)){
+                continue;
+            }
+
+            chars[tail++] = Character.toLowerCase(c);
+        }
+
+        if (tail < 2){
             return true;
         }
 
         int head = 0;
-        int tail = s.length() - 1;
-
+        tail--;
         while (head <= tail){
-            if(!Character.isLetterOrDigit(s.charAt(head))){
-                head++;
-            }else if (!Character.isLetterOrDigit(s.charAt(tail))){
-                tail--;
-            }else {
-                if(Character.toLowerCase(s.charAt(head))
-                        != Character.toLowerCase(s.charAt(tail))){
-                    return false;
-                }
-
-                head++;
-                tail--;
+            if(chars[head++] != chars[tail--]){
+                return false;
             }
         }
 
         return true;
     }
 
+    public static void main(String[] args) {
+        ValidPalindrome125 v = new ValidPalindrome125();
+
+        System.out.println(v.isPalindrome(
+                "A man, a plan, a canal: Panama"));
+    }
 
 }
