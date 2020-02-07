4c6b7bdde324c5bd437a8d63b63ed180d4810175
huangpeng <huangpeng@shalldeal.com>
2020-02-07 18:22:26 +0800

    191. 位1的个数

---
+++ leet-code/src/NumberOf1Bits191.java
@@ -0,0 +1,14 @@
+public class NumberOf1Bits191 {
+    // 191. 位1的个数 https://leetcode-cn.com/problems/number-of-1-bits/
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            // X & (X-1) 清零最低位的 1
+            n &= n - 1;
+            sum++;
+        }
+        return sum;
+    }
+}
