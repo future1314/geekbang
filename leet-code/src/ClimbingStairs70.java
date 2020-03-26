fbef46d02b6d951644b9e3238651c9f00434ef27
huangpeng <huangpeng@shalldeal.com>
2020-03-26 14:59:12 +0800

    70. 爬楼梯 再刷

--- leet-code/src/ClimbingStairs70.java
+++ leet-code/src/ClimbingStairs70.java
@@ -4,16 +4,12 @@ public class ClimbingStairs70 {
 
     public int climbStairs(int n) {
         if (n < 3) return n;
-        int first = 1;
-        int second = 2;
-        int third = 0;
-
+        int first = 1, second = 2, third = 0;
         while (2 < n--) {
             third = first + second;
             first = second;
             second = third;
         }
-
         return third;
     }
 }
